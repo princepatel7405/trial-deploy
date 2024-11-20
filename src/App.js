8add2bd33028e81bedf13af3f7fcdc2b9620430c
Prince Mendpara <prince.m.7405@gmail.com>
2024-11-20 17:25:33 +0530

    page added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<h1>Hello</h1>}/>
+        <Route path="/" element={<Home/>}/>
       </Routes>
     </div>
   );
